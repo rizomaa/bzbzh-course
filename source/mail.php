edb15d661485837f10ea19ddca92af8354c39713
Yulia <riajulia@yandex.ru>
2022-03-12 17:08:20 +0300

    Amended form message

--- source/mail.php
+++ source/mail.php
@@ -7,11 +7,14 @@ $mes = "E-mail: $email \nТекст: $message";
 $send = mail($myemail, $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
 
 if ($send == 'true') {
-    echo "<script>alert(\"Паведамленне адпраўлена / Сообщение отправлено\");</script>";
-    header('Location: index.html');
+    echo "<script>alert(\"Паведамленне адпраўлена / Сообщение отправлено\"); 
+    window.location.href="index.html"</script>";
+    
+    //header('Location: index.html');
 } else {
-    echo "<script>alert(\"Адбылася памылка, паспрабуйце яшчэ раз / Произошла ошибка, попробуйте еще раз\");</script>";
-    header('Location: index.html');
+    echo "<script>alert(\"Адбылася памылка, паспрабуйце яшчэ раз / Произошла ошибка, попробуйте еще раз\");
+    window.location.href="index.html"</script>";
+    //header('Location: index.html');
 }
 
 ?>
