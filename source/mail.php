3d2e2647e91bf950facf961309e40991fa150c35
Yulia <riajulia@yandex.ru>
2022-03-12 17:25:38 +0300

    form redirect

--- source/mail.php
+++ source/mail.php
@@ -7,13 +7,12 @@ $mes = "E-mail: $email \nТекст: $message";
 $send = mail($myemail, $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
 
 if ($send == 'true') {
-    echo "<script>alert(\"Паведамленне адпраўлена / Сообщение отправлено\"); 
-    window.location.href="index.html"</script>";
-    
+    echo "<script>alert(\"Паведамленне адпраўлена / Сообщение отправлено\");
+    location.href='http://course.bzh-bzh.by'</script>";
     //header('Location: index.html');
 } else {
     echo "<script>alert(\"Адбылася памылка, паспрабуйце яшчэ раз / Произошла ошибка, попробуйте еще раз\");
-    window.location.href="index.html"</script>";
+    location.href='http://course.bzh-bzh.by'</script>";
     //header('Location: index.html');
 }
 
