03c466c7a80b378ebd4a2f8cccb016594d3999de
Yulia <riajulia@yandex.ru>
2022-03-12 12:00:44 +0300

    form testing

--- source/mail.php
+++ source/mail.php
@@ -1,45 +1,15 @@
-$errors = '';
-$myemail = 'julia.suslennikova@gmail.com';//<-----Put Your email address here.
-if( 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-
-
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if (empty($errors))
-
-{
-
-$to = $myemail;
-
-$email_subject = "Contact form submission";
-
-$email_body = "You have received a new message. ".
-
-"Here are the details:
-
-"Email: $email_address\n Message \n $message";
-
-$headers = "From: $myemail\n";
-
-$headers .= "Reply-To: $email_address";
-
-mail($to,$email_subject,$email_body,$headers);
-
-//redirect to the 'thank you' page
-
-header('Location: index.html');
-
-}
+<?php
+// Получаем значения переменных из пришедших данных
+$myemail = 'julia.suslennikova@gmail.com';
+$email = $_POST['email'];
+$message = $_POST['message'];
+// Формируем сообщение для отправки, в нём мы соберём всё, что ввели в форме
+$mes = "E-mail: $email \nТекст: $message";
+// Пытаемся отправить письмо по заданному адресу
+// Если нужно, чтобы письма всё время уходили на ваш адрес — замените первую переменную $email на свой адрес электронной почты
+$send = mail($myemail, $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+// Если отправка прошла успешно — так и пишем
+if ($send == 'true') {echo "Сообщение отправлено";}
+// Если письмо не ушло — выводим сообщение об ошибке
+else {echo "Ой, что-то пошло не так";}
+?>
