9dea7685476482a4f8f4b5315f76cbe7ebad1e93
Yulia <riajulia@yandex.ru>
2022-03-12 16:05:00 +0300

    added email validation

---
+++ source/js/form.js
@@ -0,0 +1,29 @@
+let form = document.querySelector('.form')
+let email = form.querySelector('.form__email');
+let submit = form.querySelector('.form__submit');
+
+let emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+email.addEventListener('input', () => {
+    const invalidEmail = [];
+    email.setCustomValidity('');
+
+    if (email.value === 0) {
+        return;
+    }
+
+    const isNotPattern = emailPattern.test(email.value) === false;
+    if (isNotPattern) {
+      invalidEmail.push('E-mail уведзены няправільна / E-mail введен неверно');
+      email.style.borderColor = 'red';
+    }
+
+    if (invalidEmail.length > 0) {
+      email.setCustomValidity(invalidEmail.join('. \n'));
+      email.style.borderColor = 'red';
+    } else {
+      email.style.borderColor = '#28a745';
+    }
+    
+    email.reportValidity();
+});
--- source/mail.php
+++ source/mail.php
@@ -1,34 +1,17 @@
 <?php
-// Получаем значения переменных из пришедших данных
 $myemail = 'julia.suslennikova@gmail.com';
 $email = $_POST['email'];
 $message = $_POST['message'];
-// Формируем сообщение для отправки, в нём мы соберём всё, что ввели в форме
 $mes = "E-mail: $email \nТекст: $message";
 
-if (!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email)) {
-    $errors .= "\n Error: Invalid email address";
-}
-if( 
-    empty($_POST['email']) || 
-    empty($_POST['message'])) {
-    $errors .= "\n Error: all fields are required";
-}
-// Пытаемся отправить письмо по заданному адресу
-// Если нужно, чтобы письма всё время уходили на ваш адрес — замените первую переменную $email на свой адрес электронной почты
+$send = mail($myemail, $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
 
-if (empty($errors)) {
-    $send = mail($myemail, $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
-
-// Если отправка прошла успешно — так и пишем
 if ($send == 'true') {
-    echo "Сообщение отправлено";
+    echo "Паведамленне адпраўлена / Сообщение отправлено";
+    header('Location: index.html');
+} else {
+    echo "Адбылася памылка, паспрабуйце яшчэ раз / Произошла ошибка, попробуйте еще раз";
+    header('Location: index.html');
 }
-// Если письмо не ушло — выводим сообщение об ошибке
-else {
-    echo "Ой, что-то пошло не так";
-}
-header('Location: index.html');
-}
+
 ?>
